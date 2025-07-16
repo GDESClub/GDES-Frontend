072915608b10629066631b6a221df3ccd8d45ed3
Rishi-Gupta <guptarishiiitjee@gmail.com>
2025-07-16 17:11:05 +0530

    Games page started

--- Code/src/components/Navbar.jsx
+++ Code/src/components/Navbar.jsx
@@ -1,14 +1,25 @@
 import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 function Navbar({ onLoginClick, onSignupClick }) {
     return (
-        <div>
-            <Link to="/">Home</Link>
-            <Link to="/games">Games</Link>
-            <Link to="/leaderboard">Leaderboard</Link>
-            <Link to="/about">About</Link>
-            <button onClick={onLoginClick}>Login</button>
-            <button onClick={onSignupClick}>Sign Up</button>
+        <div className="navbar">
+            <div className="navbar-pages">
+                <Link to="/">Home</Link>
+                <Link to="/games">Games</Link>
+                <Link to="/leaderboard">Leaderboard</Link>
+                <Link to="/about">About</Link>
+            </div>
+            <div className='navbar-button'>
+                <button onClick={onLoginClick} className="cyber-button bg-red fg-white">Login
+                    <span class="glitchtext">ERROR</span>
+                    <span class="tag">R25</span>
+                </button>
+                <button onClick={onSignupClick} className="cyber-button bg-red fg-white">Sign Up
+                    <span class="glitchtext">ERROR</span>
+                    <span class="tag">R25</span>
+                </button>
+            </div>
         </div>
     );
 }
--- Code/src/pages/Games.jsx
+++ Code/src/pages/Games.jsx
@@ -1,12 +1,47 @@
 import Navbar from "../components/Navbar";
+import '../page_styles/AllGames.css';
 
-function GamesPage(){
-    return(
-        <div>
-            <Navbar/>
-            Games Page.
-        </div>
-    );
-}
+function GamesPage() {
+	const username = "Rishi Gupta";
+	const padding = 50;
+	const approxTextWidth = username.length * 8 + 100;
+
+	return (
+		<div className="GamesPage">
+			<Navbar />
+			<div className="GamesPage-Header">
+				<PlayGames title="Play Games!" />
+				<div className="logged-in-box">
+					<svg
+						width={approxTextWidth + padding}
+						height="36"
+						viewBox={`0 0 ${approxTextWidth + padding + 1} 36`}
+						fill="none"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<path
+							d={`M13 1L1 13.14V35H${approxTextWidth + padding - 11}L${approxTextWidth + padding} 24.8V1H13Z`}
+							stroke="#FF73A4"
+						/>
+						<text
+							x={(approxTextWidth + padding) / 2}
+							y="24"
+							textAnchor="middle"
+							fill="#ECF5FF"
+						>
+							Logged in as : {username}
+						</text>
+					</svg>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+const PlayGames = ({ title }) => (
+	<div className="play-games">
+		<h2>{title}</h2>
+	</div>
+);
 
 export default GamesPage;
